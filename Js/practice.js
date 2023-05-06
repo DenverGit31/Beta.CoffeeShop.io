22b96467dbf556fc9a9b46867cf6fd79e8903e7c
DenverGit <129637992+DenverGit31@users.noreply.github.com>
2023-05-06 22:29:16 +0800

    Add files via upload

--- Js/practice.js
+++ Js/practice.js
@@ -12,3 +12,8 @@ const sample32 = document.querySelector('div')
 // Adding element
 // sample.append('tag name')
  
+// local storage
+const i = document.querySelector('#samp')
+i.value = 1
+const j = i.value;
+console.log(j)
