0a9dcd69f82614aa35c6a883c3d29395e247b48b
DenverGit <129637992+DenverGit31@users.noreply.github.com>
2023-05-06 22:09:18 +0800

    Add files via upload

---
+++ Js/practice.js
@@ -0,0 +1,14 @@
+// DOM
+// GetElementById
+const sample = document.getElementById('test')
+
+// getElementByClassname
+
+const sample1  = document.getElementsByClassName('test1')
+
+// document.querySelector
+const sample32 = document.querySelector('div')
+
+// Adding element
+// sample.append('tag name')
+ 
